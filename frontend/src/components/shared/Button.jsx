6c1a7d1626d06ffecdeb7cb37aee128444039b38
Trend20 <enockomondi305@gmail.com>
2024-12-09 09:00:45 +0300

    common button

---
+++ frontend/src/components/shared/Button.jsx
@@ -0,0 +1,14 @@
+const Button = ({ children, onClick, classNames }) => {
+    return(
+        <div>
+            <button
+                onClick={onClick}
+                className={`${classNames} inline-block rounded border border-primary bg-primary px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500`}
+            >
+                {children}
+            </button>
+        </div>
+    )
+}
+
+export default Button;
